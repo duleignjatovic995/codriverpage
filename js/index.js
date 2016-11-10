2c57ff59754e721f39c179fbea9e1d9d43125793
dusan <dusan.ignjatovic@hotmail.com>
2016-11-10 10:09:18 +0100

    fixed google maps

--- js/index.js
+++ js/index.js
@@ -14,3 +14,19 @@ $(function () {
         console.log('Started')
     });
 });
+
+function initMap() {
+    var uluru = {lat: 44.7726008, lng: 20.4751682};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 10,
+        draggable: false
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+
+    marker.setMap(map);
+}
