c593e923791fba747a05c551138d6c2b67e86fd0
dusan <dusan.ignjatovic@hotmail.com>
2016-11-10 16:07:19 +0100

    Zajednicki rad u kaficu :D

--- js/index.js
+++ js/index.js
@@ -19,9 +19,9 @@ function initMap() {
     var uluru = {lat: 44.7726008, lng: 20.4751682};
 
     var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 10,
-        draggable: false
+        center: {lat: 44.7726008, lng: 20.4751682},
+        zoom: 15,
+        draggable: true
     });
     var marker = new google.maps.Marker({
         position: uluru,
@@ -30,3 +30,57 @@ function initMap() {
 
     marker.setMap(map);
 }
+
+/* jQuery for sign up form */
+
+$(document).ready(function () {
+    var datum = new Date();
+    document.getElementById("datum").innerHTML = datum.toDateString();
+});
+
+$(document).ready(function() {
+
+    $(".onclick").click(function() {
+        $("#contactdiv").css("display", "inline-block");
+    });
+    $("#contact-form #cancel-img").click(function() {
+        $(this).parent().parent().hide();
+    });
+    $("#contact-form #cancel").click(function() {
+        $(this).parent().parent().hide();
+    });
+// Contact form popup send-button click event.
+    $("#send").click(function() {
+        var name = $("#name-field").val();
+        var email = $("#email-field").val();
+        var contact = $("#contactno").val();
+        var message = $("#message").val();
+        if (name == "" || email == "" || contactno == "" || message == ""){
+            alert("Please Fill All Fields");//TODO
+        }else{
+            if (validateEmail(email)) {
+                $("#contactdiv").css("display", "none");
+            }else {
+                $("#email-field").css("border-color", "red");
+            }
+            function validateEmail(email) {
+                var filter = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
+                if (filter.test(email)) {
+                    return true;
+                }else {
+                    return false;
+                }
+            }
+        }
+    });
+// Login form popup login-button click event.
+    $("#loginbtn").click(function() {
+        var name = $("#username").val();
+        var password = $("#password").val();
+        if (username == "" || password == ""){
+            alert("Username or Password was Wrong");
+        }else{
+            $("#logindiv").css("display", "none");
+        }
+    });
+});
